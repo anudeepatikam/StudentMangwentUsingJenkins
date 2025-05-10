017303e551c23a019469772d1c48c54488a3b7bb
anudeepatikam <atikamanudeep@gmail.com>
2025-05-11 00:05:07 +0530

    first-commit

---
+++ StudentMangwentUsingJenkins/Controllers/StudentController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StudentMangwentUsingJenkins.Models;
+
+namespace StudentMangwentUsingJenkins.Controllers
+{
+    public class StudentController : Controller
+    {
+        private static List<Student> students = new List<Student>{
+            new Student{ Id=1,Name="Anudeep", Address="Mancherial"},
+            new Student{ Id=2,Name="Abhilsah", Address="Microsoft"}
+            };
+        public IActionResult Index()
+        {
+            return View(students);
+        }
+
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+
+        public  IActionResult Create(Student student)
+        {
+            student.Id = students.Count + 1;
+            students.Add(student);
+            return RedirectToAction("Index");
+        }
+
+    }
+}
